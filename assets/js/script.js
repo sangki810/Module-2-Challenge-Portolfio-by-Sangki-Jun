319e657e78e7f05b9b89f3494107f9fd38eb4079
sangki810 <113147507+sangki810@users.noreply.github.com>
2022-11-17 15:16:00 -0500

    updated work content

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,16 @@
 // DOM references
 var gameFinderEl = document.getElementById('game-finder');
+var cinemafilesEl = document.getElementById('cinemafiles');
 
 // function to handle location change after click on element
-function handleWindowLocation() {
+function handleGameFinderLocation() {
     window.location.href="https://ianmosur06.github.io/Project-1-Game-Finder-by-Adam-Larue-Sangki-Jun-and-Ian-Mosur/";
-}
+};
+
+function handleCinemafilesLocation() {
+    window.location.href="https://cinemafiles.herokuapp.com/";
+};
 
 // event listener for click on game-finder
-gameFinderEl.addEventListener("click", handleWindowLocation);
+gameFinderEl.addEventListener("click", handleGameFinderLocation);
+cinemafilesEl.addEventListener("click", handleCinemafilesLocation);
